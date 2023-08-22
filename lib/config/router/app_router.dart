aa34e864955b24f7719e139e699c72401776fd5f
Suyai Slootmans <suyaislootmans@gmail.com>
2023-08-22 11:18:57 -0300

    Estructura inicial de la aplicación

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,15 @@
+import 'package:go_router/go_router.dart';
+import 'package:forms_app/presentation/presentation.dart';
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/cubits',
+      builder: (context, state) => const CubitCounterScreen(),
+    ),
+  ],
+);
