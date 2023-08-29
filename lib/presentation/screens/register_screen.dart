50273ee35d6025b61b9c0c9d1a321fd63bcb4724
Suyai Slootmans <suyaislootmans@gmail.com>
2023-08-29 11:38:12 -0300

    diseño del campo de texto

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presentation/presentation.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -25,23 +26,36 @@ class _RegisterView extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const FlutterLogo(size: 100),
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
-              const SizedBox(height: 20),
-              FilledButton.tonalIcon(
-                onPressed: () {},
-                icon: const Icon(Icons.save),
-                label: const Text('Crear usuario'),
-              ),
-              const SizedBox(height: 20),
+            children: const [
+              FlutterLogo(size: 100),
+              _RegisterForm(),
+              SizedBox(height: 20),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      children: [
+        CustomTextFormField(),
+        const SizedBox(height: 10),
+        CustomTextFormField(),
+        const SizedBox(height: 20),
+        FilledButton.tonalIcon(
+          onPressed: () {},
+          icon: const Icon(Icons.save),
+          label: const Text('Crear usuario'),
+        ),
+      ],
+    ));
   }
 }
