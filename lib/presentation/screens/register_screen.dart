b51da65a7ca9413a8c47df5c87bad595cfca651f
Suyai Slootmans <suyaislootmans@gmail.com>
2023-09-05 13:49:08 -0300

    Mostrar errores en pantalla

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -42,84 +42,65 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatefulWidget {
+class _RegisterForm extends StatelessWidget {
   const _RegisterForm();
-
-  @override
-  State<_RegisterForm> createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final password = registerCubit.state.password;
 
     return Form(
-        key: _formKey,
         child: Column(
-          children: [
-            CustomTextFormField(
-              label: 'Nombre de usuario',
-              onChanged: (value) {
-                registerCubit.usernameChanged(value);
-                _formKey.currentState?.validate();
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) return 'Campo requqerido';
-                if (value.trim().isEmpty) return 'Campo requqerido';
-                if (value.length < 6) {
-                  return 'El usuario debe tener al menos 6 letras';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomTextFormField(
-              label: 'Correo electrónico',
-              onChanged: (value) {
-                registerCubit.emailChanged(value);
-                _formKey.currentState?.validate();
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) return 'Campo requqerido';
-                if (value.trim().isEmpty) return 'Campo requqerido';
-                final emailRegExp = RegExp(
-                  r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                );
-                if (!emailRegExp.hasMatch(value)) return 'Formato inválido';
-                return null;
-              },
-            ),
-            const SizedBox(height: 10),
-            CustomTextFormField(
-              label: 'Contraseña',
-              obscureText: true,
-              onChanged: (value) {
-                registerCubit.passwordChanged(value);
-                _formKey.currentState?.validate();
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) return 'Campo requqerido';
-                if (value.trim().isEmpty) return 'Campo requqerido';
-                if (value.length < 6) {
-                  return 'El usuario debe tener al menos 6 letras';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            FilledButton.tonalIcon(
-              onPressed: () {
-               // final isValid = _formKey.currentState!.validate();
-               // if (!isValid) return;
-                registerCubit.onSubmit();
-              },
-              icon: const Icon(Icons.save),
-              label: const Text('Crear usuario'),
-            ),
-          ],
-        ));
+      children: [
+        CustomTextFormField(
+          label: 'Nombre de usuario',
+          onChanged: registerCubit.usernameChanged,
+          errorMessage:
+              username.isPure || username.isValid ? null : 'Usuario no válido',
+        ),
+        const SizedBox(height: 10),
+        CustomTextFormField(
+          label: 'Correo electrónico',
+          onChanged: (value) {
+            registerCubit.emailChanged(value);
+          },
+          validator: (value) {
+            if (value == null || value.isEmpty) return 'Campo requqerido';
+            if (value.trim().isEmpty) return 'Campo requqerido';
+            final emailRegExp = RegExp(
+              r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+            );
+            if (!emailRegExp.hasMatch(value)) return 'Formato inválido';
+            return null;
+          },
+        ),
+        const SizedBox(height: 10),
+        CustomTextFormField(
+          label: 'Contraseña',
+          obscureText: true,
+          onChanged: (value) {
+            registerCubit.passwordChanged(value);
+          },
+          validator: (value) {
+            if (value == null || value.isEmpty) return 'Campo requqerido';
+            if (value.trim().isEmpty) return 'Campo requqerido';
+            if (value.length < 6) {
+              return 'El usuario debe tener al menos 6 letras';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 20),
+        FilledButton.tonalIcon(
+          onPressed: () {
+            registerCubit.onSubmit();
+          },
+          icon: const Icon(Icons.save),
+          label: const Text('Crear usuario'),
+        ),
+      ],
+    ));
   }
 }
