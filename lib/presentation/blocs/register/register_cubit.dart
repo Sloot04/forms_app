48ece10373b17f545a7d0bf1843772c69623a295
Suyai Slootmans <suyaislootmans@gmail.com>
2023-09-05 13:03:57 -0300

    inputs personalizados

--- lib/presentation/blocs/register/register_cubit.dart
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
+import 'package:forms_app/infrastructure/inputs.dart';
 
 part 'register_state.dart';
 
@@ -11,7 +13,11 @@ class RegisterCubit extends Cubit<RegisterFormState> {
   }
 
   void usernameChanged(String value) {
-    emit(state.copyWith(username: value));
+    final username = Username.dirty(value);
+    emit(state.copyWith(
+      username: username,
+      isValid: Formz.validate([username]),
+    ));
   }
 
   void emailChanged(String value) {
